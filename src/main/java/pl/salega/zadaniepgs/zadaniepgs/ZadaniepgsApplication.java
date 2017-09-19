2c716a7f9eb2f2c4ef284aeaa79d183202afd06e
Kamil Sałęga <kamilsalega@gmail.com>
2017-09-19 09:54:43 +0200

    Utworznie projektu

---
+++ src/main/java/pl/salega/zadaniepgs/zadaniepgs/ZadaniepgsApplication.java
@@ -0,0 +1,12 @@
+package pl.salega.zadaniepgs.zadaniepgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadaniepgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadaniepgsApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/salega/zadaniepgs/zadaniepgs/ZadaniepgsApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.salega.zadaniepgs.zadaniepgs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZadaniepgsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
